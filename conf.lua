3318e7de65c5f5a8e022869791cc5563d65e766d
geoSanchez <gsanchez@tam-c.com>
2015-09-15 16:08:00 -0500

    Initial Commit

---
+++ conf.lua
@@ -0,0 +1,10 @@
+--Configuracion
+function love.conf(t)
+  t.title = "Tutorial Scrolling Shooter" -- El titulo de la ventana de windows (String)
+  t.version = "0.9.2" -- LÖVE version
+  t.window.width = 480
+  t.window.height = 800
+
+  -- Para deguear en windows
+  t.console = true
+end
